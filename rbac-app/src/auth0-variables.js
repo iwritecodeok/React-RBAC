0b14818204eeb5b9531e82b7d002c74f7366bb1a
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-12-13 10:32:40 -0600

    added auth0-js
    integrated auth 0 js to handle basic user registration/authentication as well as store user objects

---
+++ rbac-app/src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "<YOUR_AUTH0_DOMAIN>",
+    roleUrl: "dev-m0k6vdqt.auth0.com",
+    clientId: "yEomSthZi2adRe5pMWXCHH3Ymvw4Girj",
+    callbackUrl: "http://localhost:3000/callback"
+  };
